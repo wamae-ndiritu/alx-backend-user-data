f4be4f54fe4ad58fe2abc1f0890f61024284f16e
wamae-ndiritu <wamaejoseph392@gmail.com>
2024-04-16 21:52:46 +0000

    Extract user credentials from decoded base64 auth header

--- 0x01-Basic_authentication/api/v1/auth/basic_auth.py
+++ 0x01-Basic_authentication/api/v1/auth/basic_auth.py
@@ -37,3 +37,22 @@ class BasicAuth(Auth):
             return decoded_str
         except (base64.binascii.Error, UnicodeDecodeError):
             return None
+
+    def extract_user_credentials(
+            self,
+            decoded_base64_authorization_header: str
+            ) -> (str, str):
+        """
+        Extract user credentials from
+        the decoded base64 authorization headers
+        """
+        if decoded_base64_authorization_header is None:
+            return None, None
+        if not isinstance(
+                decoded_base64_authorization_header,
+                str):
+            return None, None
+        if ':' not in decoded_base64_authorization_header:
+            return None, None
+        email, password = decoded_base64_authorization_header.split(':', 1)
+        return email, password
