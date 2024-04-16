1e6fd95b658a2556f0de62a905b2f219b8fdd59e
wamae-ndiritu <wamaejoseph392@gmail.com>
2024-04-16 19:47:59 +0000

    Error handler: Unauthorized

--- 0x01-Basic_authentication/api/v1/views/index.py
+++ 0x01-Basic_authentication/api/v1/views/index.py
@@ -24,3 +24,9 @@ def stats() -> str:
     stats = {}
     stats['users'] = User.count()
     return jsonify(stats)
+
+@app_views.route('/unauthorized', methods=['GET'], strict_slashes=False)
+def test_unauthorized() -> str:
+    """Test unauthorized error handler
+    """
+    abort(401)
