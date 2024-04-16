3a428183aa43edbdbf41ae00888a582a91155903
wamae-ndiritu <wamaejoseph392@gmail.com>
2024-04-16 19:55:11 +0000

    Error handler: Forbidden

--- 0x01-Basic_authentication/api/v1/views/index.py
+++ 0x01-Basic_authentication/api/v1/views/index.py
@@ -25,8 +25,16 @@ def stats() -> str:
     stats['users'] = User.count()
     return jsonify(stats)
 
+
 @app_views.route('/unauthorized', methods=['GET'], strict_slashes=False)
 def test_unauthorized() -> str:
     """Test unauthorized error handler
     """
     abort(401)
+
+
+@app_views.route('/forbidden', methods=['GET'], strict_slashes=False)
+def test_forbidden() -> str:
+    """Test forbidden error handler
+    """
+    abort(403)
