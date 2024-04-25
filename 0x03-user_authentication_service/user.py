4711f8f4a9043b45301a969e5619d8bad5fcc1b1
wamae-ndiritu <wamaejoseph392@gmail.com>
2024-04-25 17:42:03 +0000

    Check nullables

--- 0x03-user_authentication_service/user.py
+++ 0x03-user_authentication_service/user.py
@@ -1,4 +1,4 @@
-#!usr/bin/env python3
+#!/usr/bin/env python3
 """
 User Module
 """
@@ -14,8 +14,9 @@ class User(Base):
     User model
     """
     __tablename__ = 'users'
+
     id = Column(Integer, primary_key=True)
-    email = Column(String(250))
-    hashed_password = Column(String(250))
+    email = Column(String(250), nullable=False)
+    hashed_password = Column(String(250), nullable=False)
     session_id = Column(String(250), nullable=True)
     reset_token = Column(String(250), nullable=True)
